635d19eab773a4297c61927f7c3969f3ed369cb7
sakibstark11 <16sakib@gmail.com>
2023-01-02 23:48:05 +0600

    lint

--- s3_md5.py
+++ s3_md5.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
     start_time = perf_counter()
 
     args = parse_args()
-    main_s3_client = client("s3")
+    main_s3_client = client('s3')
     md5_hash = parse_file_md5(
         main_s3_client,
         args.bucket,
