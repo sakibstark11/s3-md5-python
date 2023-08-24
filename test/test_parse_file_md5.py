44aa2dcd3f9dd8d70bf2b29a1d11de787ad3ab69
sakibstark11 <16sakib@gmail.com>
2023-08-24 12:46:30 +0100

    test fix

--- test/test_parse_file_md5.py
+++ test/test_parse_file_md5.py
@@ -10,5 +10,5 @@ from src.s3_md5 import parse_file_md5
 def test_parse_file_md5(s3_setup: Tuple[S3Client, str, str, str]):
     '''test function'''
     s3_client, test_bucket, test_file_name, test_body = s3_setup
-    md5_hash = parse_file_md5(s3_client, test_bucket, test_file_name, 1, 1)
+    md5_hash = parse_file_md5(s3_client, test_bucket, test_file_name, 1, 2)
     assert md5_hash == md5(bytes(test_body, 'utf-8')).hexdigest()
